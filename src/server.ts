15f3f697c052e999707082444777aadc0cf5dc93
omar faruk~ <avirus170@gmail.com>
2023-06-14 23:12:04 +0600

    done[database connection and basic setup]

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config";
+
+async function databaseConnect() {
+  try {
+    await mongoose.connect("mongodb://127.0.0.1:27017/test");
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+databaseConnect();
