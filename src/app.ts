aef5962c03570fb04a0c6eb8300719d599e843f5
omar faruk~ <avirus170@gmail.com>
2023-06-17 09:51:33 +0600

    test

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
 const app: Application = express()
 
-        app.use(cors())
+app.use(cors())
 
 //parser
 app.use(express.json())
