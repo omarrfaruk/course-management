4d4a387215bb32c8753065847b0e320dfcf09cd7
omar faruk~ <avirus170@gmail.com>
2023-06-18 18:11:14 +0600

    [done]user interface and schema

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
 
-async function databaseConnect() {
+const databaseConnect = async () => {
   try {
     await mongoose.connect('mongodb://127.0.0.1:27017/test')
     app.listen(config.port, () => {
