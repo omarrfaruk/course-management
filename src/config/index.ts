544d1f92f6802c564231d37a2624572576de86a5
omar faruk~ <avirus170@gmail.com>
2023-06-20 23:52:57 +0600

    addd

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,11 @@
+/* eslint-disable no-undef */
 import dotenv from 'dotenv'
 import path from 'path'
 
 dotenv.config({ path: path.join(process.cwd(), '.env') })
 
 export default {
+  // eslint-disable-next-line no-undef
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   database_url: process.env.DATABASE_URL,
