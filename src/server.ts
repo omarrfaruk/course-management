ae63023c2005c73d7c48d03edfd372e88f74140a
omar faruk~ <avirus170@gmail.com>
2023-06-20 23:52:12 +0600

    database connection problem solved

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import config from './config'
 
 const databaseConnect = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/test')
+    await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
       console.log(`Example app listening on port ${config.port}`)
     })
