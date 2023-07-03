33f37d90c656b97d1fa256f1e5fe7b9e626e2075
omar faruk~ <avirus170@gmail.com>
2023-07-03 22:57:53 +0600

    update

--- src/app/modules/academicSemester/academicSemesterController.ts
+++ src/app/modules/academicSemester/academicSemesterController.ts
@@ -1,25 +1,50 @@
-import { NextFunction, Request, Response } from 'express';
-import { academicSemesterService } from './academicSemesterService';
-import catchAsync from '../../../shared/catchAsync';
-import sendResponse from '../../../shared/sendResponse';
+// import { NextFunction, Request, Response } from 'express';
+// import { academicSemesterService } from './academicSemesterService';
+// import catchAsync from '../../../shared/catchAsync';
+// import sendResponse from '../../../shared/sendResponse';
+// import { IPaginationOptions } from '../../../interfaces/paginationOptions';
 
-const createAcademicSemester = catchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const { ...academicSemester } = req.body;
-    const result = await academicSemesterService.createAcademicSemester(
-      academicSemester
-    );
+// const createAcademicSemester = catchAsync(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const { ...academicSemester } = req.body;
+//     const result = await academicSemesterService.createAcademicSemester(
+//       academicSemester
+//     );
 
-    sendResponse(res, {
-      success: true,
-      data: result,
-      message: 'success',
-      statusCode: 200,
-    });
-    next();
-  }
-);
+//     sendResponse(res, {
+//       success: true,
+//       data: result,
+//       message: 'success',
+//       statusCode: 200,
+//     });
+//     next();
+//   }
+// );
 
-export const academicSemesterController = {
-  createAcademicSemester,
-};
+// //get semester
+
+// const getSemester = catchAsync(
+//   async (req: Request, res: Response, next: NextFunction) => {
+//     const paginationOptions:IPaginationOptions = {
+//       page: Number(req.query.page),
+//       limit: Number(req.query.limit),
+//       sortBy: req.query.sortBy,
+//       sortOrder: req.query.sortOrder,
+//     };
+//     const result = await academicSemesterService.getAcademicSemester(
+//       paginationOptions
+//     );
+//     sendResponse(res, {
+//       success: true,
+//       data: result,
+//       message: 'success',
+//       statusCode: 200,
+//     });
+//     next();
+//   }
+// );
+
+// export const academicSemesterController = {
+//   createAcademicSemester,
+//   getSemester,
+// };
--- src/app/modules/academicSemester/academicSemesterService.ts
+++ src/app/modules/academicSemester/academicSemesterService.ts
@@ -1,21 +1,28 @@
-import ApiError from '../../../errors/ApiError';
-import { academicSemesterMapper } from './academicSemesterConstant';
-import academicSemesterUser from './academicSemesterModel';
-import { IAcademicSemester } from './academicSemisterInterface';
+// import ApiError from '../../../errors/ApiError';
+// import { IPaginationOptions } from '../../../interfaces/paginationOptions';
+// import { academicSemesterMapper } from './academicSemesterConstant';
+// import academicSemesterUser from './academicSemesterModel';
+// import { IAcademicSemester } from './academicSemisterInterface';
 
-const createAcademicSemester = async (
-  payload: IAcademicSemester
-): Promise<IAcademicSemester | null> => {
-  if (academicSemesterMapper[payload.title] !== payload.code) {
-    throw new ApiError(400, 'Invalid semester code');
-  }
-  const createdUser = await academicSemesterUser.create(payload);
-  if (!createAcademicSemester) {
-    throw new ApiError(400, 'failed to create academic user');
-  }
-  return createdUser;
-};
+// const createAcademicSemester = async (
+//   payload: IAcademicSemester
+// ): Promise<IAcademicSemester | null> => {
+//   if (academicSemesterMapper[payload.title] !== payload.code) {
+//     throw new ApiError(400, 'Invalid semester code');
+//   }
+//   const createdUser = await academicSemesterUser.create(payload);
+//   if (!createAcademicSemester) {
+//     throw new ApiError(400, 'failed to create academic user');
+//   }
+//   return createdUser;
+// };
 
-export const academicSemesterService = {
-  createAcademicSemester,
-};
+// const getAcademicSemester = async (payload:IPaginationOptions) => {
+//   const data = await academicSemesterUser.find()
+//   return data
+// };
+
+// export const academicSemesterService = {
+//   createAcademicSemester,
+//   getAcademicSemester,
+// };
